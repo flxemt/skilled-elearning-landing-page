2a2fffa0a8f9a99d867cbace97e5ffd9d4b82b43
flxemt <kudavidss@gmail.com>
2023-02-24 03:53:52 +0400

    fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -36,14 +36,14 @@ module.exports = {
     },
     backgroundImage: {
       'gradient-1': 'linear-gradient(180deg, #FF6F48 0.04%, #F02AA6 100%)',
-      'gradient-2': 'linear-gradient(180deg, #4851FF 0%, #F02AA6 99.92%)',
+      'gradient-2': 'linear-gradient(180deg, #4851FF -54.32%, #F02AA6 100%)',
       'main-gradient': 'linear-gradient(180deg, #FFFFFF 0%, #F0F1FF 100%)'
     },
     container: {
       center: true
     },
     borderRadius: {
-      btn: '28px',
+      btn: '32px',
       card: '10px',
       'card-xl': '15px'
     },
